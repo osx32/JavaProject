5a8e66cbf49e194ece6d869d3df8305f60c08fa3
Kaan Çoban <kaaan3452@gmail.com>
2025-07-05 23:35:45 +0300

    Delete deneme3 directory

--- deneme3/src/classMethods.java
+++
@@ -1,9 +0,0 @@
-public class classMethods {
-    static void myMethod(){
-        System.out.println("Hello World");
-    }
-
-    public static void main(String[] args){
-        myMethod();
-    }
-}
--- deneme3/src/objects.java
+++
@@ -1,9 +0,0 @@
-public class objects {
-    int x = 5;
-    public static void main(String[] args) {
-        objects myObj =  new objects();
-        objects myObj2 =  new objects();
-        System.out.println(myObj.x);
-        System.out.println(myObj2.x);
-    }
-}
